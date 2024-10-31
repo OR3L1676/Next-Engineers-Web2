c123a7d64ccfcb2af8b7260467e6898c4ce930c9
OR3L1676 <orelush555@gmail.com>
2024-10-31 16:17:26 +0200

    fixing access token try 3 loggin

--- backend/next-engineers-server/server.js
+++ backend/next-engineers-server/server.js
@@ -15,20 +15,17 @@ mongoose.connect(dbURI)
   .catch((err) => console.log('Failed to connect MongoDB', err));
 
 // Enable CORS
-const allowedOrigins = [
-  'https://next-engineers-web2.onrender.com', // Replace with your production frontend URL
-  'http://localhost:4173', // Local development URL
-];
+const allowedOrigins = ['https://next-engineers-web2.onrender.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true); // Allow the request
+      callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }, 
-  // credentials: true
+  },
+  credentials: true, 
 }));
 
 // Middleware
